0697f3168b3d3aa4d354b86133ee6a76694eb09b
vika.kleyko <vika.kleyko@gmail.com>
2021-12-01 17:32:12 -0800

    slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(){
+    const [value, setValue] = useState('0');
+    const [list, ] = useState([...Array(11).keys()]);
 
-export default App;
+    const handleChange = event => {
+        setValue(event.target.value);
+    }
+
+    // move to the nearest step when on mouseup event
+    const onMouseUp = event => {
+        const value = Number(event.target.value);
+        const step = Math.floor(value);
+        const decimals = Number((value - Math.floor(value)).toFixed(1));
+
+        if(decimals >= 0.5 ) {
+            setValue(step + 1);
+        } else {
+            setValue(step);
+        }
+    }
+
+    return (
+        <div className='app'>
+            <div className='wrapper'>
+                <div className='sliderContainer'>
+                <input
+                    className='slider'
+                    type='range'
+                    min='0'
+                    max='10'
+                    value={value}
+                    onChange={handleChange}
+                    onMouseUp={onMouseUp}
+                    step='0.001'
+                    list='datalist'/>
+                <div className='ticks'>
+                    {list.map(item => {
+                        return (
+                            <span
+                                key={item}
+                                className={ item - 0.5  < Number(value) && Number(value) < item + 0.5  ? 'hideTick' : 'tick'}
+                            />
+                        )
+                    })}
+                </div>
+                </div>
+                <datalist className='rangeList' id='datalist'>
+                    {list.map(item => {
+                        return (
+                        <option
+                            key={item}
+                            className={Number(value) === item ? 'focusOption' : 'rangeOption'}
+                        >
+                            {item}
+                        </option>
+                        )
+                    })}
+                </datalist>
+            </div>
+        </div>
+    );
+}
